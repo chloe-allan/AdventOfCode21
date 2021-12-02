f588ba9c1faf3374d81c0bd5b20c3efcace77659
chloe.allan <chloe.allan@just-eat.com>
2021-12-02 22:33:07 +0000

    Day 1 done

---
+++ Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logFile = File.ReadAllLines("input.txt");
+            var logList = logFile.Select(int.Parse).ToList();
+
+            var increaseValues = logList
+                .Select((value, index) => new {value, index})
+                .Where(x => x.index > 0 && x.value > logList[x.index - 1]);
+
+            var result = increaseValues.Count();
+
+            Console.WriteLine(result);
+        }
+    }
+}
