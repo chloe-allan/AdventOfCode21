c3f636f8b1454a4194e0e8a3f2551f2871f56287
chloe.allan <chloe.allan@just-eat.com>
2021-12-02 22:56:15 +0000

    And I missed part 2 of day 1 lol

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -12,13 +12,41 @@ namespace Day1
             var logFile = File.ReadAllLines("input.txt");
             var logList = logFile.Select(int.Parse).ToList();
 
-            var increaseValues = logList
+            var part1Result = Part1(logList);
+            Console.WriteLine($"Part 1 result: {part1Result}");
+
+            var part2Result = Part2(logList);
+            Console.WriteLine($"Part 2 result (sliding window): {part2Result}");
+        }
+
+        private static int Part1(List<int> input)
+        {
+            var increaseValues = input
                 .Select((value, index) => new {value, index})
-                .Where(x => x.index > 0 && x.value > logList[x.index - 1]);
+                .Where(x => x.index > 0 && x.value > input[x.index - 1]);
 
-            var result = increaseValues.Count();
+           return increaseValues.Count();
+        }
 
-            Console.WriteLine(result);
+        private static int Part2(List<int> input)
+        {
+            var trackingWindow = -1;
+            var increaseValues = input
+                .Select((value, index) => new {value, index})
+                .Where(x =>
+                {
+                    if (x.index >= input.Count() - 2) return false;
+
+                    var slidingWindowValue = x.value + input[x.index + 1] + input[x.index + 2];
+
+                    if (x.index == 0) trackingWindow = slidingWindowValue;
+                    
+                    var res = slidingWindowValue > trackingWindow;
+                    trackingWindow = slidingWindowValue;
+                    return res;
+                });
+
+            return increaseValues.Count();
         }
     }
 }
